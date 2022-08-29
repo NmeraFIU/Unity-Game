f40be9820011e2fa58dc8fefaa6a4b79253e7656
Norberto Merani <nmera003@fiu.edu>
2022-08-28 21:36:38 -0400

    Added double jump mechanic.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int exJumps = 2;
+    public LayerMask groundLayer;
+    public Transform feet;
+    bool isGrounded;
+    int jumpCount;
+    float jumpCoolDown;
+    float jumpPower = 7.0f;
+
     private Rigidbody2D rb;
 
     private Vector3 respawnPoint;
@@ -19,16 +27,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         float dirX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector3(dirX * 5f, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector3(0, 7f, 0);
+            //rb.velocity = new Vector3(0, 7f, 0);
+            Jump();
         }
+        CheckGrounded();
         if (Input.GetButtonUp("Jump"))
         {
             rb.velocity = new Vector3(0, -2f, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = respawnPoint;
         }
     }
 
@@ -36,18 +52,46 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Damage"))
         {
+            //*****Player dying and respawning*****
             Debug.Log("You died!");
             transform.position = respawnPoint;
         } else if (collision.gameObject.CompareTag("Checkpoint"))
         {
+            //I believe this is redundant code. Will worry about deleting later.
             Debug.Log(collision.gameObject.transform.position);
             Debug.Log("Checkpoint reached!");
             respawnPoint = collision.gameObject.transform.position;
         }
     }
 
+    void Jump()
+    {
+        if (isGrounded || jumpCount < exJumps)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            jumpCount++;
+        }
+    }
+
+    void CheckGrounded()
+    {
+        if (Physics2D.OverlapCircle(feet.position, 0.5f, groundLayer))
+        {
+            isGrounded = true;
+            jumpCount = 0;
+            jumpCoolDown = Time.time + 0.2f;
+        } else if (Time.time < jumpCoolDown)
+        {
+            isGrounded = true;
+        } else
+        {
+            isGrounded = false;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //*****update player's spawn point at checkpoint*****
         if (collision.gameObject.CompareTag("Checkpoint"))
         {
             Debug.Log(collision.gameObject.transform.position);
